b301cee86246529965ecce735cca0e9e24df09ca
Catalin Dumitru <colin.dumitru@gmail.com>
2014-01-19 18:52:07 +0200

    config changes

--- src/main/java/com/edec/Main.java
+++ src/main/java/com/edec/Main.java
@@ -10,7 +10,7 @@ import org.apache.jena.fuseki.FusekiCmd;
 public class Main {
 
     public static void main(String[] args) {
-        FusekiCmd.main("--mem", "--port=" + System.getenv("PORT"), "--pages=./pages", "/edec");
+        FusekiCmd.main("--mem", "--port=" + System.getenv("PORT"), "--pages=`pwd`/pages", "/edec");
     }
 
 }
