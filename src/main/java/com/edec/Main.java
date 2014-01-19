eefc05c77c016999e39105c87178670a2752ab7d
Catalin Dumitru <colin.dumitru@gmail.com>
2014-01-19 17:58:18 +0200

    trying different port

--- src/main/java/com/edec/Main.java
+++ src/main/java/com/edec/Main.java
@@ -10,7 +10,7 @@ import org.apache.jena.fuseki.FusekiCmd;
 public class Main {
 
     public static void main(String[] args) {
-        FusekiCmd.main("--mem", "--port=80", "/edec");
+        FusekiCmd.main("--mem", "--port=" + System.getenv("PORT"), "/edec");
     }
 
 }
